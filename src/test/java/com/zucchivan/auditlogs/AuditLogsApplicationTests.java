c298a2d942c1a09c881ebbb79ba946a27e34ac1f
ivan <zucchivan@gmail.com>
2019-10-22 10:31:17 +0300

    First commit

---
+++ src/test/java/com/zucchivan/auditlogs/AuditLogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zucchivan.auditlogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditLogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
